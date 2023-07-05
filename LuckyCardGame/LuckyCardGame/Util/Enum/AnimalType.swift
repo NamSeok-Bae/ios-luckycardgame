e2a2e188503cc5660106bb703498d194a1c3c1cc
NamSeok-Bae <qoskatjr2@gmail.com>
2023-07-05 15:28:24 +0900

    add: AnimalType enum 추가

---
+++ LuckyCardGame/LuckyCardGame/Util/Enum/AnimalType.swift
@@ -0,0 +1,12 @@
+//
+//  AnimalType.swift
+//  LuckyCardGame
+//
+//  Created by 배남석 on 2023/07/05.
+//
+
+import Foundation
+
+enum AnimalType: String, CaseIterable {
+    case dog = "🐶", cat = "🐱", cow = "🐮"
+}
