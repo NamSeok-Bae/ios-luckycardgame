88808e131892ecc7f527c0a32397d5548ebfaad5
NamSeok-Bae <qoskatjr2@gmail.com>
2023-07-05 15:37:35 +0900

    add: Card 클래스 생성 및 구현

--- LuckyCardGame/LuckyCardGame/Model/Class/Card.swift
+++ LuckyCardGame/LuckyCardGame/Model/Class/Card.swift
@@ -5,8 +5,30 @@
 //  Created by 배남석 on 2023/07/05.
 //
 
-import Foundation
+import UIKit
 
-final class Card {
+protocol CardInfomation {
+    var animal: String { get }
+    var number: Int { get }
+    var back: Bool { get }
+}
+
+final class Card: CardInfomation, CustomStringConvertible {
+    var animal: String
+    var number: Int
+    var back: Bool = false
+    var image: UIImage? = nil
+    var description: String {
+        return "\(animal)\(String(format: "%02d", number))"
+    }
     
+    init(animalType: AnimalType, number: Int) {
+        self.animal = animalType.rawValue
+        self.number = number
+    }
+    
+    init(animal: String, number: Int) {
+        self.animal = animal
+        self.number = number
+    }
 }
