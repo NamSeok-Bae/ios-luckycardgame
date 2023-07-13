1d50c0bdecd6187d6b7a01d7a6f7f0a4f19ed450
NamSeok-Bae <qoskatjr2@gmail.com>
2023-07-13 18:06:06 +0900

    add TargetType Enum 추가

---
+++ LuckyCardGame/LuckyCardGame/Util/Enum/TargetType.swift
@@ -0,0 +1,17 @@
+//
+//  TargetType.swift
+//  LuckyCardGame
+//
+//  Created by 배남석 on 2023/07/13.
+//
+
+import Foundation
+
+enum TargetType: Int, CaseIterable {
+    case first = 0
+    case second
+    case third
+    case fourth
+    case fifth
+    case ground
+}
