daf07e1d79ae3690882be70521365f2fee8bbe76
NamSeok-Bae <qoskatjr2@gmail.com>
2023-07-13 18:08:05 +0900

    feat: ValidatorTests Test Case 추가

--- LuckyCardGame/ValidatorTests/ValidatorTests.swift
+++ LuckyCardGame/ValidatorTests/ValidatorTests.swift
@@ -9,7 +9,49 @@ import XCTest
 @testable import 럭키카드
 
 final class ValidatorTests: XCTestCase {
+    var validator: DefaultValidator!
     
+    override func setUpWithError() throws {
+        validator = DefaultValidator()
+    }
+
+    override func tearDownWithError() throws {
+        validator = nil
+    }
+    
+    func test_플레이어_덱에서_같은_숫자_3장_검증하기() {
+        var playerDeck = [DefaultLuckyCard(.dog, .one), DefaultLuckyCard(.cat, .one), DefaultLuckyCard(.cow, .one)]
+        XCTAssertTrue(validator.validateThreeSameCardInDeck(playerDeck),
+                      "Validator의 덱에서 같은 숫자 3장 검증하기 결과값이 다름.")
+        
+        playerDeck = [DefaultLuckyCard(.dog, .one), DefaultLuckyCard(.cat, .one), DefaultLuckyCard(.cow, .two)]
+        XCTAssertFalse(validator.validateThreeSameCardInDeck(playerDeck),
+                       "Validator의 덱에서 같은 숫자 3장 검증하기 결과값이 다름.")
+    }
+    
+    func test_플레이가_뽑은_카드의_숫자가_모두_같은지_검증하기() {
+        var flippedCards = [DefaultLuckyCard]()
+        XCTAssertFalse(validator.validateCurrentFlippedCards(flippedCards),
+                      "Validator의 뒤집은 카드들이 모두 같은지 검증하기 결과값이 다름.")
+        flippedCards = [DefaultLuckyCard(.dog, .one)]
+        XCTAssertTrue(validator.validateCurrentFlippedCards(flippedCards),
+                      "Validator의 뒤집은 카드들이 모두 같은지 검증하기 결과값이 다름.")
+        
+        flippedCards = [DefaultLuckyCard(.dog, .one), DefaultLuckyCard(.cat, .one)]
+        XCTAssertTrue(validator.validateCurrentFlippedCards(flippedCards),
+                      "Validator의 뒤집은 카드들이 모두 같은지 검증하기 결과값이 다름.")
+        
+        flippedCards = [DefaultLuckyCard(.dog, .one), DefaultLuckyCard(.cat, .two)]
+        XCTAssertFalse(validator.validateCurrentFlippedCards(flippedCards),
+                      "Validator의 뒤집은 카드들이 모두 같은지 검증하기 결과값이 다름.")
+        
+        flippedCards = [DefaultLuckyCard(.dog, .one), DefaultLuckyCard(.cat, .one), DefaultLuckyCard(.cow, .one)]
+        XCTAssertTrue(validator.validateCurrentFlippedCards(flippedCards),
+                      "Validator의 뒤집은 카드들이 모두 같은지 검증하기 결과값이 다름.")
+        flippedCards = [DefaultLuckyCard(.dog, .one), DefaultLuckyCard(.cat, .one), DefaultLuckyCard(.cow, .two)]
+        XCTAssertFalse(validator.validateCurrentFlippedCards(flippedCards),
+                      "Validator의 뒤집은 카드들이 모두 같은지 검증하기 결과값이 다름.")
+    }
 
     func testExample() throws {
         // This is an example of a functional test case.
