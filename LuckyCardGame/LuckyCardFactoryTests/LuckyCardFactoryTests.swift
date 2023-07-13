c4246d138f7d802e17c77b01fa1d7dc603a99e60
NamSeok-Bae <qoskatjr2@gmail.com>
2023-07-13 17:55:59 +0900

    feat: LuckyCardFactory Test Case 구현

--- LuckyCardGame/LuckyCardFactoryTests/LuckyCardFactoryTests.swift
+++ LuckyCardGame/LuckyCardFactoryTests/LuckyCardFactoryTests.swift
@@ -9,6 +9,40 @@ import XCTest
 @testable import 럭키카드
 
 final class LuckyCardFactoryTests: XCTestCase {
+    var luckyCardFactory: LuckyCardFactory!
+
+    override func setUpWithError() throws {
+        luckyCardFactory = DefaultLuckyCardFactory()
+    }
+
+    override func tearDownWithError() throws {
+        luckyCardFactory = nil
+    }
+    
+    func test_카드팩토리_덱_생성() {
+        luckyCardFactory.createDeck()
+        
+        XCTAssertEqual(luckyCardFactory.getDeck().count, 36, "카드 팩토리에서 덱 생성 함수가 올바르지 않음.")
+        
+        var deckIndex = 0;
+        for i in AnimalType.allCases {
+            for j in CardNumberType.allCases {
+                XCTAssertEqual(luckyCardFactory.getDeck()[deckIndex], DefaultLuckyCard(i, j), "카드 팩토리에서 생성한 카드가 올바르지 않음.")
+                deckIndex += 1
+            }
+        }
+    }
+    
+    func test_카드팩토리_덱_반환() {
+        luckyCardFactory.createDeck()
+        let deck = luckyCardFactory.returnDeck()
+        let deckDescription = deck.map { $0.description }
+        
+        XCTAssertEqual(deckDescription,
+                       ["🐶01", "🐶02", "🐶03", "🐶04", "🐶05", "🐶06", "🐶07", "🐶08", "🐶09", "🐶10", "🐶11", "🐶12", "🐱01", "🐱02", "🐱03", "🐱04", "🐱05", "🐱06", "🐱07", "🐱08", "🐱09", "🐱10", "🐱11", "🐱12", "🐮01", "🐮02", "🐮03", "🐮04", "🐮05", "🐮06", "🐮07", "🐮08", "🐮09", "🐮10", "🐮11", "🐮12"],
+                       "카드 팩토리에서 생성한 카드 반환값이 결과값과 다름.")
+    }
+
     func testExample() throws {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
