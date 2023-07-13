cfefca0736857756b28d8c2cd33a714acc2de9dd
NamSeok-Bae <qoskatjr2@gmail.com>
2023-07-13 18:02:46 +0900

    feat: Player에 Sort, Flipped, DeFlipped 기능 추가

--- LuckyCardGame/LuckyCardGame/Model/Class/Player.swift
+++ LuckyCardGame/LuckyCardGame/Model/Class/Player.swift
@@ -11,6 +11,9 @@ protocol Player: AnyObject {
     func setBoard(_ board: Board)
     func getBoard() -> Board
     func setDeck(_ deck: [DefaultLuckyCard])
+    func sortDeck()
+    func flippedCard(_ flippedCardType: FlippedCardType) -> DefaultLuckyCard
+    func deFlippedCard(_ flippedCards: [DefaultLuckyCard])
 }
 
 class DefaultPlayer: Player {
@@ -34,5 +37,17 @@ class DefaultPlayer: Player {
     
     func setDeck(_ deck: [DefaultLuckyCard]) {
         self.board.setDeck(deck)
+    }
+    
+    func sortDeck() {
+        board.sortDeck()
+    }
+    
+    func flippedCard(_ flippedCardType: FlippedCardType) -> DefaultLuckyCard {
+        board.flippedCard(flippedCardType == .maxNumber ? board.getDeck().count - 1 : 0)
+    }
+    
+    func deFlippedCard(_ flippedCards: [DefaultLuckyCard]) {
+        board.deFlippedCard(flippedCards)
     }
 }
