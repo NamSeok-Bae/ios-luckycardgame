70e7f72c547bc55e3a81fb1e3610f5551d090b87
NamSeok-Bae <qoskatjr2@gmail.com>
2023-07-13 18:05:48 +0900

    add: FlippedCardType Enum 추가

---
+++ LuckyCardGame/LuckyCardGame/Util/Enum/FlippedCardType.swift
@@ -0,0 +1,13 @@
+//
+//  FlippedCardType.swift
+//  LuckyCardGame
+//
+//  Created by 배남석 on 2023/07/13.
+//
+
+import Foundation
+
+enum FlippedCardType: CaseIterable {
+    case minNumber
+    case maxNumber
+}
