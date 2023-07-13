c63e671aa662d1904d3b18c6f0d8ef64e7328093
NamSeok-Bae <qoskatjr2@gmail.com>
2023-07-13 17:58:41 +0900

    feat: LuckCardTests 테스트 케이스 추가

--- LuckyCardGame/LuckyCardTests/LuckyCardTests.swift
+++ LuckyCardGame/LuckyCardTests/LuckyCardTests.swift
@@ -9,6 +9,33 @@ import XCTest
 @testable import 럭키카드
 
 final class LuckyCardTests: XCTestCase {
+    var luckyCard: DefaultLuckyCard!
+
+    override func setUpWithError() throws {
+        luckyCard = DefaultLuckyCard()
+    }
+
+    override func tearDownWithError() throws {
+        luckyCard = nil
+    }
+    
+    func test_럭키카드_초기화() {
+        luckyCard.setInformation(.dog, .one)
+        XCTAssertEqual(luckyCard, DefaultLuckyCard(.dog, .one), "럭키카드 초기화가 결과값과 다름.")
+        XCTAssertEqual(luckyCard.getAnimal(), "🐶", "럭키카드 초기화가 결과값과 다름.")
+        XCTAssertEqual(luckyCard.getNumber(), 1, "럭키카드 초기화가 결과값과 다름.")
+        
+        var newLuckyCard = DefaultLuckyCard(.cat, .eight)
+        XCTAssertEqual(newLuckyCard, DefaultLuckyCard(.cat, .eight), "럭키카드 초기화가 결과값과 다름.")
+        XCTAssertEqual(newLuckyCard.getAnimal(), "🐱", "럭키카드 초기화가 결과값과 다름.")
+        XCTAssertEqual(newLuckyCard.getNumber(), 8, "럭키카드 초기화가 결과값과 다름.")
+        
+        newLuckyCard = DefaultLuckyCard(animal: "🐶", number: 2)
+        XCTAssertEqual(newLuckyCard, DefaultLuckyCard(.dog, .two), "럭키카드 초기화가 결과값과 다름.")
+        XCTAssertEqual(newLuckyCard.getAnimal(), "🐶", "럭키카드 초기화가 결과값과 다름.")
+        XCTAssertEqual(newLuckyCard.getNumber(), 2, "럭키카드 초기화가 결과값과 다름.")
+    }
+
     func testExample() throws {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
