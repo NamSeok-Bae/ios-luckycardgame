6c7932038d89dea5b6975fd57c40f02c717ae690
NamSeok-Bae <qoskatjr2@gmail.com>
2023-07-13 18:07:46 +0900

    add: EnumTests 추가

---
+++ LuckyCardGame/EnumTests/EnumTests.swift
@@ -0,0 +1,28 @@
+//
+//  EnumTests.swift
+//  EnumTests
+//
+//  Created by 배남석 on 2023/07/13.
+//
+
+import XCTest
+@testable import 럭키카드
+
+final class EnumTests: XCTestCase {
+    
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        // Any test you write for XCTest can be annotated as throws and async.
+        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
+        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
