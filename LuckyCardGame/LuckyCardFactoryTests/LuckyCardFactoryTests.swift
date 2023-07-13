f5c36f9ca37bba95bd8672d90fe0ac3c1883f619
NamSeok-Bae <qoskatjr2@gmail.com>
2023-07-13 17:55:36 +0900

    add: LuckyCaradFactoryTests 추가

---
+++ LuckyCardGame/LuckyCardFactoryTests/LuckyCardFactoryTests.swift
@@ -0,0 +1,27 @@
+//
+//  LuckyCardFactoryTests.swift
+//  LuckyCardFactoryTests
+//
+//  Created by 배남석 on 2023/07/12.
+//
+
+import XCTest
+@testable import 럭키카드
+
+final class LuckyCardFactoryTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        // Any test you write for XCTest can be annotated as throws and async.
+        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
+        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
